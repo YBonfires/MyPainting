bbe90b578bb2ba132c23749003ddf5f21bf57133
yy <850894694@qq.com>
2020-06-18 17:44:11 +0800

    6.18

--- app/src/main/java/com/example/mypainting/BackAlertDialog.java
+++ app/src/main/java/com/example/mypainting/BackAlertDialog.java
@@ -13,13 +13,6 @@ public class BackAlertDialog {
     public static BackAlertDialog getInstance() {
         return new BackAlertDialog();
     }
-
-    /**
-     * 带有确认取消按钮的自定义dialog
-     *
-     * @param context 上下文
-     * @param message 显示的信息
-     */
     public static void showConfirmDialog(Context context, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         final AlertDialog alertDialog = builder.create();
@@ -54,23 +47,9 @@ public class BackAlertDialog {
         this.onButtonClickListener = onButtonClickListener;
     }
 
-    /**
-     * 按钮点击回调接口
-     */
     public interface OnButtonClickListener {
-        /**
-         * 确定按钮点击回调方法
-         *
-         * @param dialog 当前 AlertDialog，传入它是为了在调用的地方对 dialog 做操作，比如 dismiss()
-         *               也可以在该工具类中直接  dismiss() 掉，就不用将 AlertDialog 对象传出去了
-         */
         void onPositiveButtonClick(AlertDialog dialog);
 
-        /**
-         * 取消按钮点击回调方法
-         *
-         * @param dialog 当前AlertDialog
-         */
         void onNegativeButtonClick(AlertDialog dialog);
     }
 }
